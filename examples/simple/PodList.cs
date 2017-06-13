f6433ca252cd91cd62fad533a5f94dc92736a7f3
Brendan Burns <brendan.d.burns@gmail.com>
2017-06-12 22:54:30 -0700

    Add some example code.

---
+++ examples/simple/PodList.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+using k8s;
+
+namespace simple
+{
+    class PodList
+    {
+        static void Main(string[] args)
+        {
+            IKubernetes client = new Kubernetes();
+            client.BaseUri = new Uri("http://localhost:8001");
+            var listTask = client.ListNamespacedPodWithHttpMessagesAsync("default");
+            listTask.Wait();
+            var list = listTask.Result.Body;
+            foreach (var item in list.Items) {
+                Console.WriteLine(item.Metadata.Name);
+            }
+        }
+    }
+}
