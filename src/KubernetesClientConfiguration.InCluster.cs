7096b1560440e8dc9f4cd42b6de0f55bf5193f1a
Boshi Lian <farmer1992@gmail.com>
2017-10-21 21:37:38 +0800

    extract path to shared const

--- src/KubernetesClientConfiguration.InCluster.cs
+++ src/KubernetesClientConfiguration.InCluster.cs
@@ -1,14 +1,14 @@
 using System;
 using System.IO;
-using System.Security.Cryptography.X509Certificates;
 using k8s.Exceptions;
 
 namespace k8s
 {
     public partial class KubernetesClientConfiguration
     {
-        private const string ServiceAccountTokenKey = "token";
-        private const string ServiceAccountRootCAKey = "ca.crt";
+        private const string ServiceaccountPath = "/var/run/secrets/kubernetes.io/serviceaccount/";
+        private const string ServiceAccountTokenKeyFileName = "token";
+        private const string ServiceAccountRootCAKeyFileName = "ca.crt";
 
         public static KubernetesClientConfiguration InClusterConfig()
         {
@@ -21,8 +21,8 @@ namespace k8s
                     "unable to load in-cluster configuration, KUBERNETES_SERVICE_HOST and KUBERNETES_SERVICE_PORT must be defined");
             }
 
-            var token = File.ReadAllText("/var/run/secrets/kubernetes.io/serviceaccount/" + ServiceAccountTokenKey);
-            var rootCAFile = "/var/run/secrets/kubernetes.io/serviceaccount/" + ServiceAccountRootCAKey;
+            var token = File.ReadAllText(Path.Combine(ServiceaccountPath, ServiceAccountTokenKeyFileName));
+            var rootCAFile = Path.Combine(ServiceaccountPath, ServiceAccountRootCAKeyFileName);
 
             return new KubernetesClientConfiguration
             {
