10e7e2ea7b73c779a733290810c2e255b614c521
Boshi Lian <farmer1992@gmail.com>
2018-01-18 21:08:30 +0800

    force patch to strong type

---
+++ src/V1Patch.cs
@@ -0,0 +1,55 @@
+using System;
+using Microsoft.AspNetCore.JsonPatch;
+using Newtonsoft.Json;
+
+namespace k8s.Models
+{
+    internal class V1PathJsonConverter : JsonConverter
+    {
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+            serializer.Serialize(writer, (value as V1Patch)?.Content);
+        }
+
+        // no read patch object supported at the moment
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue,
+            JsonSerializer serializer)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool CanConvert(Type objectType)
+        {
+            return objectType == typeof(V1Patch);
+        }
+    }
+
+    [JsonConverter(typeof(V1PathJsonConverter))]
+    public partial class V1Patch
+    {
+
+        public enum PathType
+        {
+            JsonPatch,
+            MergePatch,
+            StrategicMergePatch,
+        }
+
+        public PathType Type { get; private set; }
+
+        public V1Patch(IJsonPatchDocument jsonPatch) : this((object) jsonPatch)
+        {
+        }
+
+        partial void CustomInit()
+        {
+            if (Content is IJsonPatchDocument)
+            {
+                Type = PathType.JsonPatch;
+                return;
+            }
+
+            throw new NotSupportedException();
+        }
+    }
+}
