a283efd4fdf1495eaa62a97a3f9164f0ffedbda2
Frederik Carlier <frederik.carlier@quamotion.mobi>
2018-02-14 23:56:32 +0100

    Add Extensions properties Add as, as-groups, as-user-extra properties to UserCredentials

--- src/KubeConfigModels/K8SConfiguration.cs
+++ src/KubeConfigModels/K8SConfiguration.cs
@@ -4,10 +4,17 @@ namespace k8s.KubeConfigModels
     using YamlDotNet.Serialization;
 
     /// <summary>
-    /// kubeconfig configuration model
+    /// kubeconfig configuration model. Holds the information needed to build connect to remote
+    /// Kubernetes clusters as a given user.
     /// </summary>
+    /// <remarks>
+    /// Should be kept in sync with https://github.com/kubernetes/kubernetes/blob/master/staging/src/k8s.io/client-go/tools/clientcmd/api/v1/types.go
+    /// </remarks>
     public class K8SConfiguration
     {
+        /// <summary>
+        /// Gets or sets general information to be use for CLI interactions
+        /// </summary>
         [YamlMember(Alias = "preferences")]
         public IDictionary<string, object> Preferences{ get; set; }
 
@@ -17,16 +24,34 @@ namespace k8s.KubeConfigModels
         [YamlMember(Alias = "kind")]
         public string Kind { get; set; }
 
+        /// <summary>
+        /// Gets or sets the name of the context that you would like to use by default.
+        /// </summary>
         [YamlMember(Alias = "current-context")]
         public string CurrentContext { get; set; }
 
+        /// <summary>
+        /// Gets or sets a map of referencable names to context configs.
+        /// </summary>
         [YamlMember(Alias = "contexts")]
         public IEnumerable<Context> Contexts { get; set; } = new Context[0];
 
+        /// <summary>
+        /// Gets or sets a map of referencable names to cluster configs.
+        /// </summary>
         [YamlMember(Alias = "clusters")]
         public IEnumerable<Cluster> Clusters { get; set; } = new Cluster[0];
 
+        /// <summary>
+        /// Gets or sets a map of referencable names to user configs
+        /// </summary>
         [YamlMember(Alias = "users")]
         public IEnumerable<User> Users { get; set; } = new User[0];
+
+        /// <summary>
+        /// Gets or sets additional information. This is useful for extenders so that reads and writes don't clobber unknown fields.
+        /// </summary>
+        [YamlMember(Alias = "extensions")]
+        public IDictionary<string, dynamic> Extensions { get; set; }
     }
 }
