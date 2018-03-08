ca803786533b2eaf02e842ad6fff4c93eb8eee28
Brendan Burns <bburns@microsoft.com>
2018-03-07 21:33:30 -0800

    Add a utility for YAML loading.

---
+++ src/Yaml.cs
@@ -0,0 +1,34 @@
+using System.IO;
+using System.Threading.Tasks;
+using YamlDotNet.Serialization;
+using YamlDotNet.Serialization.NamingConventions;
+
+namespace k8s
+{
+    /// <summary>
+    /// This is a utility class that helps you load objects from YAML files.
+    /// </summary>
+
+    public class Yaml {
+        public static async Task<T> LoadFromStreamAsync<T>(Stream stream) {
+            var reader = new StreamReader(stream);
+            var content = await reader.ReadToEndAsync();
+            return LoadFromString<T>(content);
+        }
+
+        public static async Task<T> LoadFromFileAsync<T> (string file) {
+            using (FileStream fs = File.Open(file, FileMode.Open, FileAccess.Write, FileShare.None)) { 
+                return await LoadFromStreamAsync<T>(fs);
+            }
+        }
+
+        public static T LoadFromString<T>(string content) {
+            var deserializer = 
+                new DeserializerBuilder()
+                .WithNamingConvention(new CamelCaseNamingConvention())
+                .Build();
+            var obj = deserializer.Deserialize<T>(content);
+            return obj;
+        }
+    }
+}
