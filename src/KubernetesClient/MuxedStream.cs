097abd880c2ea5dce9c68aa37d40f6e7015f17d1
Frederik Carlier <frederik.carlier@quamotion.mobi>
2018-04-24 14:14:28 +0200

    Add ChannelIndex enumeration, and some documentation

--- src/KubernetesClient/MuxedStream.cs
+++ src/KubernetesClient/MuxedStream.cs
@@ -3,12 +3,27 @@ using System.IO;
 
 namespace k8s
 {
+    /// <summary>
+    /// A <see cref="Stream"/> which reads/writes from a specific channel using a <see cref="StreamDemuxer" />.
+    /// </summary>
     public class MuxedStream : Stream
     {
         private ByteBuffer inputBuffer;
         private byte? outputIndex;
         private StreamDemuxer muxer;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MuxedStream"/> class.
+        /// </summary>
+        /// <param name="muxer">
+        /// The <see cref="StreamDemuxer"/> to use to read from/write to the underlying stream.
+        /// </param>
+        /// <param name="inputBuffer">
+        /// The <see cref="inputBuffer"/> to read from.
+        /// </param>
+        /// <param name="outputIndex">
+        /// The index of the channel to which to write.
+        /// </param>
         public MuxedStream(StreamDemuxer muxer, ByteBuffer inputBuffer, byte? outputIndex)
         {
             this.inputBuffer = inputBuffer;
@@ -22,20 +37,26 @@ namespace k8s
             this.muxer = muxer ?? throw new ArgumentNullException(nameof(muxer));
         }
 
+        /// <inheritdoc/>
         public override bool CanRead => this.inputBuffer != null;
 
+        /// <inheritdoc/>
         public override bool CanSeek => false;
 
+        /// <inheritdoc/>
         public override bool CanWrite => this.outputIndex != null;
 
+        /// <inheritdoc/>
         public override long Length => throw new NotSupportedException();
 
+        /// <inheritdoc/>
         public override long Position
         {
             get => throw new NotSupportedException();
             set => throw new NotSupportedException();
         }
 
+        /// <inheritdoc/>
         public override void Write(byte[] buffer, int offset, int count)
         {
             if (this.outputIndex == null)
@@ -48,6 +69,7 @@ namespace k8s
             }
         }
 
+        /// <inheritdoc/>
         public override int Read(byte[] buffer, int offset, int count)
         {
             if (this.inputBuffer == null)
@@ -60,16 +82,19 @@ namespace k8s
             }
         }
 
+        /// <inheritdoc/>
         public override void Flush()
         {
             throw new NotSupportedException();
         }
 
+        /// <inheritdoc/>
         public override long Seek(long offset, SeekOrigin origin)
         {
             throw new NotSupportedException();
         }
 
+        /// <inheritdoc/>
         public override void SetLength(long value)
         {
             throw new NotSupportedException();
