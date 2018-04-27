1c07966d72cbebf7e82ee64ea037b2cb543faf23
Frederik Carlier <frederik.carlier@quamotion.mobi>
2018-04-27 06:14:38 +0200

    Add ChannelIndex enumeration, and some documentation (#142)

--- src/KubernetesClient/StreamDemuxer.cs
+++ src/KubernetesClient/StreamDemuxer.cs
@@ -9,6 +9,18 @@ using System.Threading.Tasks;
 
 namespace k8s
 {
+    /// <summary>
+    /// <para>
+    ///     The <see cref="StreamDemuxer"/> allows you to interact with processes running in a container in a Kubernetes pod. You can start an exec or attach command
+    ///     by calling <see cref="Kubernetes.WebSocketNamespacedPodExecAsync(string, string, IEnumerable{string}, string, bool, bool, bool, bool, Dictionary{string, List{string}}, CancellationToken)"/>
+    ///     or <see cref="Kubernetes.WebSocketNamespacedPodAttachAsync(string, string, string, bool, bool, bool, bool, Dictionary{string, List{string}}, CancellationToken)"/>. These methods
+    ///     will return you a <see cref="WebSocket"/> connection.
+    /// </para>
+    /// <para>
+    ///     Kubernetes 'multiplexes' multiple channels over this <see cref="WebSocket"/> connection, such as standard input, standard output and standard error. The <see cref="StreamDemuxer"/>
+    ///     allows you to extract individual <see cref="Stream"/>s from this <see cref="WebSocket"/> class. You can then use these streams to send/receive data from that process.
+    /// </para>
+    /// </summary>
     public class StreamDemuxer : IDisposable
     {
         private readonly WebSocket webSocket;
@@ -16,6 +28,12 @@ namespace k8s
         private readonly CancellationTokenSource cts = new CancellationTokenSource();
         private Task runLoop;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="StreamDemuxer"/> class.
+        /// </summary>
+        /// <param name="webSocket">
+        /// A <see cref="WebSocket"/> which contains a multiplexed stream, such as the <see cref="WebSocket"/> returned by the exec or attach commands.
+        /// </param>
         public StreamDemuxer(WebSocket webSocket)
         {
             this.webSocket = webSocket ?? throw new ArgumentNullException(nameof(webSocket));
@@ -23,11 +41,15 @@ namespace k8s
 
         public event EventHandler ConnectionClosed;
 
+        /// <summary>
+        /// Starts reading the data sent by the server.
+        /// </summary>
         public void Start()
         {
             this.runLoop = this.RunLoop(this.cts.Token);
         }
 
+        /// <inheritdoc/>
         public void Dispose()
         {
             try
@@ -45,6 +67,35 @@ namespace k8s
             }
         }
 
+        /// <summary>
+        /// Gets a <see cref="Stream"/> which allows you to read to and/or write from a remote channel.
+        /// </summary>
+        /// <param name="inputIndex">
+        /// The index of the channel from which to read.
+        /// </param>
+        /// <param name="outputIndex">
+        /// The index of the channel to which to write.
+        /// </param>
+        /// <returns>
+        /// A <see cref="Stream"/> which allows you to read/write to the requested channels.
+        /// </returns>
+        public Stream GetStream(ChannelIndex? inputIndex, ChannelIndex? outputIndex)
+        {
+            return GetStream((byte?)inputIndex, (byte?)outputIndex);
+        }
+
+        /// <summary>
+        /// Gets a <see cref="Stream"/> which allows you to read to and/or write from a remote channel.
+        /// </summary>
+        /// <param name="inputIndex">
+        /// The index of the channel from which to read.
+        /// </param>
+        /// <param name="outputIndex">
+        /// The index of the channel to which to write.
+        /// </param>
+        /// <returns>
+        /// A <see cref="Stream"/> which allows you to read/write to the requested channels.
+        /// </returns>
         public Stream GetStream(byte? inputIndex, byte? outputIndex)
         {
             if (inputIndex != null && !this.buffers.ContainsKey(inputIndex.Value))
@@ -60,6 +111,53 @@ namespace k8s
             return new MuxedStream(this, inputBuffer, outputIndex);
         }
 
+        /// <summary>
+        /// Directly writes data to a channel.
+        /// </summary>
+        /// <param name="index">
+        /// The index of the channel to which to write.
+        /// </param>
+        /// <param name="buffer">
+        /// The buffer from which to read data.
+        /// </param>
+        /// <param name="offset">
+        /// The offset at which to start reading.
+        /// </param>
+        /// <param name="count">
+        /// The number of bytes to read.
+        /// </param>
+        /// <param name="cancellationToken">
+        /// A <see cref="CancellationToken"/> which can be used to cancel the asynchronous operation.
+        /// </param>
+        /// <returns>
+        /// A <see cref="Task"/> which represents the asynchronous operation.
+        /// </returns>
+        public Task Write(ChannelIndex index, byte[] buffer, int offset, int count, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            return Write((byte)index, buffer, offset, count, cancellationToken);
+        }
+
+        /// <summary>
+        /// Directly writes data to a channel.
+        /// </summary>
+        /// <param name="index">
+        /// The index of the channel to which to write.
+        /// </param>
+        /// <param name="buffer">
+        /// The buffer from which to read data.
+        /// </param>
+        /// <param name="offset">
+        /// The offset at which to start reading.
+        /// </param>
+        /// <param name="count">
+        /// The number of bytes to read.
+        /// </param>
+        /// <param name="cancellationToken">
+        /// A <see cref="CancellationToken"/> which can be used to cancel the asynchronous operation.
+        /// </param>
+        /// <returns>
+        /// A <see cref="Task"/> which represents the asynchronous operation.
+        /// </returns>
         public async Task Write(byte index, byte[] buffer, int offset, int count, CancellationToken cancellationToken = default(CancellationToken))
         {
             byte[] writeBuffer = ArrayPool<byte>.Shared.Rent(count + 1);
