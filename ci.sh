9c23efc75da004a0585dfb7e30d6c6817135c0fe
Frederik Carlier <frederik.carlier@quamotion.mobi>
2018-05-07 11:35:53 +0200

    Push to NuGet feeds

--- ci.sh
+++ ci.sh
@@ -6,8 +6,14 @@ set -e
 # Ensure no compile errors in all projects
 find . -name *.csproj -exec dotnet build {} \;
 
+# Create the NuGet package
+cd src/KubernetesClient/
+dotnet pack -c Release
+cd ../..
+
 # Execute Unit tests
 cd tests/KubernetesClient.Tests
 dotnet restore
 dotnet test
 
+cd ../..
