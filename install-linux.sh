86abfc1b7cde62ca3c74f8d7960c4c7ba8ecb031
Frederik Carlier <frederik.carlier@quamotion.mobi>
2018-06-01 07:57:10 +0200

    Update to .NET Core 2.1 RTM (#166)

--- install-linux.sh
+++ install-linux.sh
@@ -5,7 +5,7 @@ curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microso
 sudo mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg
 sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/microsoft-ubuntu-trusty-prod trusty main" > /etc/apt/sources.list.d/dotnetdev.list'
 sudo apt-get -qq update
-sudo apt-get install -y dotnet-sdk-2.1.300-rc1-008673
+sudo apt-get install -y dotnet-sdk-2.1
 
 echo 'Installing kubecl'
 curl -Lo kubectl https://storage.googleapis.com/kubernetes-release/release/v1.9.0/bin/linux/amd64/kubectl
