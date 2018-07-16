0d365dc6da766ab880697ec85056ecfa9fde0b84
Rohan Nagar <rohan.nagar@utexas.edu>
2018-07-16 22:52:27 +0000

    Move Fractions library inside project

---
+++ src/KubernetesClient/Fractions/Formatter/DefaultFractionFormatProvider.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Fractions.Formatter {
+    /// <summary>
+    /// Default <see cref="Fraction.ToString()"/> formatter.
+    /// </summary>
+    public class DefaultFractionFormatProvider : IFormatProvider {
+        /// <summary>
+        /// Singleton instance
+        /// </summary>
+        public static readonly IFormatProvider Instance = new DefaultFractionFormatProvider();
+
+        object IFormatProvider.GetFormat(Type formatType) {
+            return formatType == typeof (Fraction) 
+                ? DefaultFractionFormatter.Instance
+                : null;
+        }
+    }
+}
---
+++ src/KubernetesClient/Fractions/InvalidNumberException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Fractions {
+    /// <summary>
+    /// Exception that will be thrown if an argument contains not a number (NaN) or is infinite.
+    /// </summary>
+    public class InvalidNumberException : ArithmeticException {
+#pragma warning disable 1591
+        public InvalidNumberException() {}
+        public InvalidNumberException(string message) : base(message) {}
+        public InvalidNumberException(string message, Exception innerException) : base(message, innerException) {}
+#pragma warning restore 1591
+    }
+}
