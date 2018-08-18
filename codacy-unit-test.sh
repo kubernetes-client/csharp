8d92ef323797979fa96904c46e14d2d7b5c9394d
Sergio Sisternes <sesispla@outlook.com>
2018-08-18 17:25:25 +0200

    Changed output format

--- codacy-unit-test.sh
+++ codacy-unit-test.sh
@@ -1,9 +1,9 @@
 # Execute Unit tests
 cd tests/KubernetesClient.Tests
 dotnet restore
-dotnet test --no-restore --no-build /p:CollectCoverage=true /p:ExcludeByFile=\"*/generated/**/*.cs\" /p:CoverletOutputFormat=lcov /p:CoverletOutput=./coverage.xml
+dotnet test --no-restore --no-build /p:CollectCoverage=true /p:ExcludeByFile=\"*/generated/**/*.cs\" /p:CoverletOutputFormat=cobertura /p:CoverletOutput=./coverage.xml
 echo Uploading coverage...
-java -jar ~/codacy-coverage-reporter-assembly-latest.jar report -l CSharp --forceLanguage -r ./coverage.xml
+java -jar ~/codacy-coverage-reporter-assembly-latest.jar report -l CSharp -r ./coverage.xml
 if [[ $? != 0 ]]; then
     exit 1
 fi
