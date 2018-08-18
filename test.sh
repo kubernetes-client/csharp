0985320e39b0509571af09b5b3c46347f5e145b8
Sergio Sisternes <sesispla@outlook.com>
2018-08-18 03:15:16 +0200

    Trying to use relative paths

--- test.sh
+++ test.sh
@@ -1,8 +1,8 @@
 # Execute Unit tests
 cd tests/KubernetesClient.Tests
 dotnet restore
-dotnet test --no-restore --no-build /p:CollectCoverage=true /p:CoverletOutputFormat=opencover /p:CoverletOutput=./opencover.xml
-./../../tools/csmacnz.Coveralls --opencov -i ./opencover.xml --repoToken $COVERALLS_REPO_TOKEN --commitId $TRAVIS_COMMIT --commitBranch $TRAVIS_BRANCH --commitAuthor "$REPO_COMMIT_AUTHOR" --commitEmail "$REPO_COMMIT_AUTHOR_EMAIL" --commitMessage "$REPO_COMMIT_MESSAGE" --jobId $TRAVIS_JOB_ID  --serviceName "travis-ci"  --useRelativePaths
+dotnet test --no-restore --no-build /p:CollectCoverage=true /p:ExcludeByFile=\"*/generated/*.cs\" /p:CoverletOutputFormat=opencover /p:CoverletOutput=./opencover.xml
+./../../tools/csmacnz.Coveralls --opencov -i ./opencover.xml --useRelativePaths --repoToken $COVERALLS_REPO_TOKEN --commitId $TRAVIS_COMMIT --commitBranch $TRAVIS_BRANCH --commitAuthor "$REPO_COMMIT_AUTHOR" --commitEmail "$REPO_COMMIT_AUTHOR_EMAIL" --commitMessage "$REPO_COMMIT_MESSAGE" --jobId $TRAVIS_JOB_ID  --serviceName "travis-ci"
 if [[ $? != 0 ]]; then
     exit 1
 fi
