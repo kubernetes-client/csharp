2550c2341e69e425570cc163ce22348a08f9db6a
Sergio Sisternes <sesispla@outlook.com>
2018-08-18 03:38:17 +0200

    Exclude generated files

--- test.sh
+++ test.sh
@@ -1,7 +1,7 @@
 # Execute Unit tests
 cd tests/KubernetesClient.Tests
 dotnet restore
-dotnet test --no-restore --no-build /p:CollectCoverage=true /p:ExcludeByFile=\"*/generated/*.cs\" /p:CoverletOutputFormat=opencover /p:CoverletOutput=./opencover.xml
+dotnet test --no-restore --no-build /p:CollectCoverage=true /p:ExcludeByFile=\"*/generated/**/*.cs\" /p:CoverletOutputFormat=opencover /p:CoverletOutput=./opencover.xml
 ./../../tools/csmacnz.Coveralls --opencov -i ./opencover.xml --useRelativePaths --repoToken $COVERALLS_REPO_TOKEN --commitId $TRAVIS_COMMIT --commitBranch $TRAVIS_BRANCH --commitAuthor "$REPO_COMMIT_AUTHOR" --commitEmail "$REPO_COMMIT_AUTHOR_EMAIL" --commitMessage "$REPO_COMMIT_MESSAGE" --jobId $TRAVIS_JOB_ID  --serviceName "travis-ci"
 if [[ $? != 0 ]]; then
     exit 1
