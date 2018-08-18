837d6ee5e574aea12f74daafc82c8f06996b928f
Sergio Sisternes <sesispla@outlook.com>
2018-08-18 16:53:18 +0200

    Force language

--- codacy-unit-test.sh
+++ codacy-unit-test.sh
@@ -2,7 +2,7 @@
 cd tests/KubernetesClient.Tests
 dotnet restore
 dotnet test --no-restore --no-build /p:CollectCoverage=true /p:ExcludeByFile=\"*/generated/**/*.cs\" /p:CoverletOutputFormat=lcov /p:CoverletOutput=./coverage.xml
-java -jar ~/codacy-coverage-reporter-assembly-latest.jar report -l csharp -r ./coverage.xml
+java -jar ~/codacy-coverage-reporter-assembly-latest.jar report -l CSharp --forceLanguage -r ./coverage.xml
 if [[ $? != 0 ]]; then
     exit 1
 fi
