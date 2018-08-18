593800aac7d68cf901c57c0cea420276cc4745f9
Sergio Sisternes <sesispla@outlook.com>
2018-08-18 12:09:14 +0200

    More codacy

--- codacy-unit-test.sh
+++ codacy-unit-test.sh
@@ -2,7 +2,6 @@
 cd tests/KubernetesClient.Tests
 dotnet restore
 dotnet test --no-restore --no-build /p:CollectCoverage=true /p:ExcludeByFile=\"*/generated/**/*.cs\" /p:CoverletOutputFormat=lcov /p:CoverletOutput=./coverage.xml
-codacy-coverage-reporter report -l csharp -r ./coverage.xml
 if [[ $? != 0 ]]; then
     exit 1
 fi
