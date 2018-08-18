81eb75c88c204256908daace4940defaf5b07ed0
Sergio Sisternes <sesispla@outlook.com>
2018-08-18 02:00:57 +0200

    Added coveralls setup

--- ci.sh
+++ ci.sh
@@ -9,12 +9,7 @@ find . -name *.csproj -exec dotnet build {} \;
 # Create the NuGet package
 cd src/KubernetesClient/
 dotnet pack -c Release
-cd ../..
 
-# Execute Unit tests
-cd tests/KubernetesClient.Tests
-dotnet restore
-dotnet test --no-restore --no-build /p:CollectCoverage=true /p:CoverletOutputFormat=lcov /p:CoverletOutput=./lcov.info
 if [[ $? != 0 ]]; then
     exit 1
 fi
