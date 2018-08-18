8d03196684c7b2a2d554d990e563ea37c873285d
Sergio Sisternes <sesispla@outlook.com>
2018-08-18 18:10:23 +0200

    Simplified upload name

--- linux-unit-test.sh
+++ linux-unit-test.sh
@@ -3,7 +3,7 @@ cd tests/KubernetesClient.Tests
 dotnet restore
 dotnet test --no-restore --no-build /p:CollectCoverage=true /p:CoverletOutputFormat=cobertura /p:CoverletOutput=./coverage.xml
 echo Uploading coverage...
-java -jar ~/codacy-coverage-reporter-assembly-latest.jar report -l CSharp -r ./coverage.xml
+java -jar ~/codacy-coverage-reporter-assembly-latest.jar report -l CSharp -r coverage.xml
 if [[ $? != 0 ]]; then
     exit 1
 fi
