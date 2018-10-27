355d4a3927ab876c841c4fce7aec3906792c628d
Brendan Burns <bburns@microsoft.com>
2018-10-26 23:04:22 -0700

    Rev OS X SDK for dotnet (#217)

--- install-osx.sh
+++ install-osx.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 echo 'Installing .NET Core...'
 
-wget https://download.microsoft.com/download/8/8/5/88544F33-836A-49A5-8B67-451C24709A8F/dotnet-sdk-2.1.300-osx-x64.pkg -O ~/dotnet-sdk-2.1.300-osx-x64.pkg
-sudo installer -pkg ~/dotnet-sdk-2.1.300-osx-x64.pkg -target /
+wget https://download.visualstudio.microsoft.com/download/pr/38102737-cb48-46c2-8f52-fb7102b50ae7/d81958d71c3c2679796e1ecfbd9cc903/dotnet-sdk-2.1.403-osx-x64.pkg -O ~/dotnet-sdk-2.1.403-osx-x64.pkg
+
+sudo installer -pkg ~/dotnet-sdk-2.1.403-osx-x64.pkg -target /
 
 # https://github.com/dotnet/cli/issues/2544
 ln -s /usr/local/share/dotnet/dotnet /usr/local/bin/
