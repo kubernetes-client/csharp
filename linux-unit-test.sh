b40acd9fa0f3d9463f7045ddf8b50692395289b1
Sergio Sisternes <sesispla@outlook.com>
2019-01-05 14:30:40 +0100

    More path issues with Travis...

--- linux-unit-test.sh
+++ linux-unit-test.sh
@@ -4,7 +4,7 @@ dotnet restore
 dotnet test --no-restore --no-build /p:CollectCoverage=true /p:CoverletOutputFormat=cobertura /p:CoverletOutput=./coverage.xml
 cd ../..
 echo Uploading coverage...
-java -jar ./tools/codacy-coverage-reporter.jar report -l CSharp -r ./tests/KubernetesClient.Tests/coverage.xml; 
+java -jar ./tools/codacy-coverage-reporter.jar report -l CSharp -r tests/KubernetesClient.Tests/coverage.xml; 
 if [[ $? != 0 ]]; then
     exit 1
 fi
