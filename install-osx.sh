0ea30a99b1f902f6df955e02ace06330bb8343ae
Sergio Sisternes <sesispla@outlook.com>
2019-01-05 15:04:24 +0100

    Removing unnecesary coverage from osx

--- install-osx.sh
+++ install-osx.sh
@@ -11,9 +11,3 @@ sudo installer -pkg ~/dotnet-sdk-2.1.403-osx-x64.pkg -target /
 
 # https://github.com/dotnet/cli/issues/2544
 ln -s /usr/local/share/dotnet/dotnet /usr/local/bin/
-
-echo 'Installing code coverage tools..'
-
-brew install jq
-mkdir -p tools
-wget -O ./tools/codacy-coverage-reporter.jar $(curl https://api.github.com/repos/codacy/codacy-coverage-reporter/releases/latest | jq -r .assets[0].browser_download_url)
--- osx-unit-test.sh
+++ osx-unit-test.sh
@@ -1,10 +1,8 @@
 # Execute Unit tests
 cd tests/KubernetesClient.Tests
 dotnet restore
-dotnet test --no-restore --no-build /p:CollectCoverage=true /p:CoverletOutputFormat=cobertura /p:CoverletOutput=./coverage.xml
-cd ../..
-echo Uploading coverage...
-java -jar ./tools/codacy-coverage-reporter.jar report -l CSharp -r tests/KubernetesClient.Tests/coverage.xml; 
+dotnet test --no-restore --no-build
+
 if [[ $? != 0 ]]; then
     exit 1
 fi
