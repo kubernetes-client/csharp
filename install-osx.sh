5d8e2abe6ac1622a8a57fe60274952c7dedfca78
Brendan Burns <bburns@microsoft.com>
2019-02-20 16:49:48 -0800

    Update dotnet on OS X to 2.2

--- install-osx.sh
+++ install-osx.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 echo 'Installing .NET Core...'
 
-wget https://download.visualstudio.microsoft.com/download/pr/38102737-cb48-46c2-8f52-fb7102b50ae7/d81958d71c3c2679796e1ecfbd9cc903/dotnet-sdk-2.1.403-osx-x64.pkg -O ~/dotnet-sdk-2.1.403-osx-x64.pkg
+wget https://download.visualstudio.microsoft.com/download/pr/3fb2ae01-c8c5-4d0a-9102-31c8c3386bc5/94b144257db9c52405d7f7e03adc31a9/dotnet-sdk-2.2.104-osx-gs-x64.pkg -O ~/dotnet-sdk-2.2.104-osx-gs-x64.pkg
 
-sudo installer -pkg ~/dotnet-sdk-2.1.403-osx-x64.pkg -target /
+sudo installer -pkg ~/dotnet-sdk-2.2.104-osx-gs-x64.pkg -target /
 
 # https://github.com/dotnet/cli/issues/2544
 ln -s /usr/local/share/dotnet/dotnet /usr/local/bin/
