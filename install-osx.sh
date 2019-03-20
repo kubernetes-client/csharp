60792c81c1bb921feca23bcaf7ca67dad4124bfe
Frederik Carlier <frederik.carlier@quamotion.mobi>
2019-03-20 16:43:53 +0100

    Update CI dependencies

--- install-osx.sh
+++ install-osx.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 echo 'Installing .NET Core...'
 
-wget https://download.visualstudio.microsoft.com/download/pr/3fb2ae01-c8c5-4d0a-9102-31c8c3386bc5/94b144257db9c52405d7f7e03adc31a9/dotnet-sdk-2.2.104-osx-gs-x64.pkg -O ~/dotnet-sdk-2.2.104-osx-gs-x64.pkg
+wget https://download.visualstudio.microsoft.com/download/pr/4850aa8f-44a9-4c4a-9961-f18aa4d90ceb/07d790444f3ba6b412a76d6f1aced338/dotnet-sdk-2.2.105-osx-x64.pkg -O ~/dotnet-sdk-2.2.105-osx-x64.pkg
 
-sudo installer -pkg ~/dotnet-sdk-2.2.104-osx-gs-x64.pkg -target /
+sudo installer -pkg ~/dotnet-sdk-2.2.105-osx-x64.pkg -target /
 
 # https://github.com/dotnet/cli/issues/2544
 ln -s /usr/local/share/dotnet/dotnet /usr/local/bin/
