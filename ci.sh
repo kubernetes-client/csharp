7b8a36c99ed16573d0948db3572d1f6bffbeb0fb
Frederik Carlier <frederik.carlier@quamotion.mobi>
2019-03-26 00:29:12 +0100

    Add Linux & macOS leg to CI, add code coverage (#264)

--- ci.sh
+++ ci.sh
@@ -14,6 +14,26 @@ cd ../..
 # Execute Unit tests
 cd tests/KubernetesClient.Tests
 dotnet restore
-dotnet test
+# Save the test results to a file
+# Collect code coverage of the KuberetsClient assembly, but exclude the
+# auto-generated models from the coverage reports.
+dotnet test \
+    -l "trx;LogFileName=KubernetesClient.Tests.xunit.trx" \
+    /p:CollectCoverage=true \
+    /p:Include="[KubernetesClient]*" \
+    /p:Exclude="[KubernetesClient]k8s.Models.*" \
+    /p:Exclude="[KubernetesClient]k8s.Internal.*" \
+    /p:CoverletOutputFormat="opencover" \
+    /p:CoverletOutput="KubernetesClient.Tests.opencover.xml"
 
-cd ../..
+cd ..
+echo Generating Code Coverage reports
+export PATH="$PATH:$HOME/.dotnet/tools"
+export DOTNET_ROOT=$(dirname $(realpath $(which dotnet))) # https://github.com/dotnet/cli/issues/9114#issuecomment-401670622
+dotnet tool install --global dotnet-reportgenerator-globaltool --version 4.0.15
+reportgenerator "-reports:**/*.opencover.xml" "-targetdir:coveragereport" "-reporttypes:HTMLInline;Cobertura"
+
+ls coveragereport
+ls coveragereport/Cobertura.xml
+
+cd ..
