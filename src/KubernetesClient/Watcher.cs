752db1efe99ac1519dd1bf21d9da29b93927384a
Boshi Lian <farmer1992@gmail.com>
2019-10-02 15:23:08 -0700

    fix and add testcase for async watch

--- src/KubernetesClient/Watcher.cs
+++ src/KubernetesClient/Watcher.cs
@@ -179,6 +179,7 @@ namespace k8s
         /// create a watch object from a call to api server with watch=true
         /// </summary>
         /// <typeparam name="T">type of the event object</typeparam>
+        /// <typeparam name="L">type of the HttpOperationResponse object</typeparam>
         /// <param name="response">the api response</param>
         /// <param name="onEvent">a callback when any event raised from api server</param>
         /// <param name="onError">a callbak when any exception was caught during watching</param>
@@ -186,7 +187,7 @@ namespace k8s
         /// The action to invoke when the server closes the connection.
         /// </param>
         /// <returns>a watch object</returns>
-        public static Watcher<T> Watch<T>(this HttpOperationResponse<T> response,
+        public static Watcher<T> Watch<T, L>(this HttpOperationResponse<L> response,
             Action<WatchEventType, T> onEvent,
             Action<Exception> onError = null,
             Action onClosed = null)
