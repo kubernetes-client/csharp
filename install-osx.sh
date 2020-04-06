944622dd65abfd1c5475e1e1463f6624c626a5c2
Andy Kernahan <andrew.kernahan@gmail.com>
2020-04-06 10:51:48 +0100

    Upgrade to .NET Core SDK 3.1. (#395)
    * Upgrade to .NET Core SDK 3.1.
    
    * Add workaround for setup-dotnet not supporting SxS dotnet installs.
    
    * Specify explicit fetch depth of 0 for Nerdbank.GitVersioning.
    
    * Make step names consistent across workflows.
    
    * Coerce robocopy's exit code when it does not indicate an error.
    
    * Make robocopy as quiet as possible.

--- install-osx.sh
+++ install-osx.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
 echo 'Installing .NET Core...'
 
-wget https://download.visualstudio.microsoft.com/download/pr/4850aa8f-44a9-4c4a-9961-f18aa4d90ceb/07d790444f3ba6b412a76d6f1aced338/dotnet-sdk-2.2.105-osx-x64.pkg -O ~/dotnet-sdk-2.2.105-osx-x64.pkg
+wget https://download.microsoft.com/download/3/a/3/3a3bda26-560d-4d8e-922e-6f6bc4553a84/dotnet-runtime-2.0.9-osx-x64.pkg -O ~/dotnet-runtime-2.0.9-osx-x64.pkg
+wget https://download.visualstudio.microsoft.com/download/pr/9d3edcf8-2da1-42eb-a30f-54d629c8f13f/2e967304f8f3543c7329fd53d292d076/dotnet-runtime-2.1.17-osx-x64.pkg -O ~/dotnet-runtime-2.1.17-osx-x64.pkg
+wget https://download.visualstudio.microsoft.com/download/pr/905598d0-17a3-4b42-bf13-c5a69d7aac87/853aff73920dcb013c09a74f05da7f6a/dotnet-sdk-3.1.201-osx-x64.pkg -O ~/dotnet-sdk-3.1.201-osx-x64.pkg
 
-sudo installer -pkg ~/dotnet-sdk-2.2.105-osx-x64.pkg -target /
+sudo installer -pkg ~/dotnet-runtime-2.0.9-osx-x64.pkg -target /
+sudo installer -pkg ~/dotnet-runtime-2.1.17-osx-x64.pkg -target /
+sudo installer -pkg ~/dotnet-sdk-3.1.201-osx-x64.pkg -target /
 
 # https://github.com/dotnet/cli/issues/2544
 ln -s /usr/local/share/dotnet/dotnet /usr/local/bin/
