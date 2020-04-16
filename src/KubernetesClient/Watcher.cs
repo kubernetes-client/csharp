33e0b28138d6e7580b1335d7bcfff466d5821a43
admilazz <32494910+admilazz@users.noreply.github.com>
2020-04-16 10:52:46 -0700

    Add WatchEventType.Bookmark (#414)
    * Add WatchEventType.Bookmark
    
    * Avoid changing value order

--- src/KubernetesClient/Watcher.cs
+++ src/KubernetesClient/Watcher.cs
@@ -10,15 +10,27 @@ using Microsoft.Rest.Serialization;
 
 namespace k8s
 {
+    /// <summary>Describes the type of a watch event.</summary>
     public enum WatchEventType
     {
+        /// <summary>Emitted when an object is created, modified to match a watch's filter, or when a watch is first opened.</summary>
         [EnumMember(Value = "ADDED")] Added,
 
+        /// <summary>Emitted when an object is modified.</summary>
         [EnumMember(Value = "MODIFIED")] Modified,
 
+        /// <summary>Emitted when an object is deleted or modified to no longer match a watch's filter.</summary>
         [EnumMember(Value = "DELETED")] Deleted,
 
-        [EnumMember(Value = "ERROR")] Error
+        /// <summary>Emitted when an error occurs while watching resources. Most commonly, the error is 410 Gone which indicates that
+        /// the watch resource version was outdated and events were probably lost. In that case, the watch should be restarted.
+        /// </summary>
+        [EnumMember(Value = "ERROR")] Error,
+
+        /// <summary>Bookmarks may be emitted periodically to update the resource version. The object will
+        /// contain only the resource version.
+        /// </summary>
+        [EnumMember(Value = "BOOKMARK")] Bookmark,
     }
 
     public class Watcher<T> : IDisposable
