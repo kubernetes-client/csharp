a4ff002bde75a09cbca0dbb896616218bafe25de
admilazz <32494910+admilazz@users.noreply.github.com>
2020-05-18 15:11:36 -0700

    Add convenient model extensions (#405)
    * Add useful model extensions
    
    * Rename a function
    
    * Add more model extensions; add unit tests
    
    * Add a couple more extensions requested in PR
    
    * Rebase on master
    
    * Remove unused using statement

--- src/KubernetesClient/ModelExtensions.cs
+++ src/KubernetesClient/ModelExtensions.cs
@@ -1,7 +1,458 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Net;
 
 namespace k8s.Models
 {
+    /// <summary>Adds convenient extensions for Kubernetes objects.</summary>
+    public static class ModelExtensions
+    {
+        /// <summary>Adds the given finalizer to a Kubernetes object if it doesn't already exist.</summary>
+        /// <returns>Returns true if the finalizer was added and false if it already existed.</returns>
+        public static bool AddFinalizer(this IMetadata<V1ObjectMeta> obj, string finalizer)
+        {
+            if (string.IsNullOrEmpty(finalizer))
+            {
+                throw new ArgumentNullException(nameof(finalizer));
+            }
+            if (EnsureMetadata(obj).Finalizers == null)
+            {
+                obj.Metadata.Finalizers = new List<string>();
+            }
+            if (!obj.Metadata.Finalizers.Contains(finalizer))
+            {
+                obj.Metadata.Finalizers.Add(finalizer);
+                return true;
+            }
+            return false;
+        }
+
+        /// <summary>Extracts the Kubernetes API group from the <see cref="IKubernetesObject.ApiVersion"/>.</summary>
+        public static string ApiGroup(this IKubernetesObject obj)
+        {
+            if (obj == null)
+            {
+                throw new ArgumentNullException(nameof(obj));
+            }
+            if (obj.ApiVersion != null)
+            {
+                int slash = obj.ApiVersion.IndexOf('/');
+                return slash < 0 ? string.Empty : obj.ApiVersion.Substring(0, slash);
+            }
+            return null;
+        }
+
+        /// <summary>Extracts the Kubernetes API version (excluding the group) from the <see cref="IKubernetesObject.ApiVersion"/>.</summary>
+        public static string ApiGroupVersion(this IKubernetesObject obj)
+        {
+            if (obj == null)
+            {
+                throw new ArgumentNullException(nameof(obj));
+            }
+            if (obj.ApiVersion != null)
+            {
+                int slash = obj.ApiVersion.IndexOf('/');
+                return slash < 0 ? obj.ApiVersion : obj.ApiVersion.Substring(slash+1);
+            }
+            return null;
+        }
+
+        /// <summary>Splits the Kubernetes API version into the group and version.</summary>
+        public static (string, string) ApiGroupAndVersion(this IKubernetesObject obj)
+        {
+            string group, version;
+            GetApiGroupAndVersion(obj, out group, out version);
+            return (group, version);
+        }
+
+        /// <summary>Splits the Kubernetes API version into the group and version.</summary>
+        public static void GetApiGroupAndVersion(this IKubernetesObject obj, out string group, out string version)
+        {
+            if (obj == null)
+            {
+                throw new ArgumentNullException(nameof(obj));
+            }
+            if (obj.ApiVersion == null)
+            {
+                group = version = null;
+            }
+            else
+            {
+                int slash = obj.ApiVersion.IndexOf('/');
+                if (slash < 0) (group, version) = (string.Empty, obj.ApiVersion);
+                else (group, version) = (obj.ApiVersion.Substring(0, slash), obj.ApiVersion.Substring(slash+1));
+            }
+        }
+
+        /// <summary>Gets the continuation token version of a Kubernetes list.</summary>
+        public static string Continue(this IMetadata<V1ListMeta> list) => list.Metadata?.ContinueProperty;
+
+        /// <summary>Ensures that the <see cref="V1ListMeta"/> metadata field is set, and returns it.</summary>
+        public static V1ListMeta EnsureMetadata(this IMetadata<V1ListMeta> obj)
+        {
+            if (obj == null)
+            {
+                throw new ArgumentNullException(nameof(obj));
+            }
+            if (obj.Metadata == null)
+            {
+                obj.Metadata = new V1ListMeta();
+            }
+            return obj.Metadata;
+        }
+
+        /// <summary>Gets the resource version of a Kubernetes list.</summary>
+        public static string ResourceVersion(this IMetadata<V1ListMeta> list) => list.Metadata?.ResourceVersion;
+
+        /// <summary>Adds an owner reference to the object. No attempt is made to ensure the reference is correct or fits with the
+        /// other references.
+        /// </summary>
+        public static void AddOwnerReference(this IMetadata<V1ObjectMeta> obj, V1OwnerReference ownerRef)
+        {
+            if (ownerRef == null)
+            {
+                throw new ArgumentNullException(nameof(ownerRef));
+            }
+            if (EnsureMetadata(obj).OwnerReferences == null)
+            {
+                obj.Metadata.OwnerReferences = new List<V1OwnerReference>();
+            }
+            obj.Metadata.OwnerReferences.Add(ownerRef);
+        }
+
+        /// <summary>Gets the annotations of a Kubernetes object.</summary>
+        public static IDictionary<string, string> Annotations(this IMetadata<V1ObjectMeta> obj) => obj.Metadata?.Annotations;
+
+        /// <summary>Gets the creation time of a Kubernetes object, or null if it hasn't been created yet.</summary>
+        public static DateTime? CreationTimestamp(this IMetadata<V1ObjectMeta> obj) => obj.Metadata?.CreationTimestamp;
+
+        /// <summary>Gets the deletion time of a Kubernetes object, or null if it hasn't been scheduled for deletion.</summary>
+        public static DateTime? DeletionTimestamp(this IMetadata<V1ObjectMeta> obj) => obj.Metadata?.DeletionTimestamp;
+
+        /// <summary>Ensures that the <see cref="V1ObjectMeta"/> metadata field is set, and returns it.</summary>
+        public static V1ObjectMeta EnsureMetadata(this IMetadata<V1ObjectMeta> obj)
+        {
+            if (obj == null)
+            {
+                throw new ArgumentNullException(nameof(obj));
+            }
+            if (obj.Metadata == null)
+            {
+                obj.Metadata = new V1ObjectMeta();
+            }
+            return obj.Metadata;
+        }
+
+        /// <summary>Gets the <see cref="V1ObjectMeta.Finalizers"/> of a Kubernetes object.</summary>
+        public static IList<string> Finalizers(this IMetadata<V1ObjectMeta> obj) => obj.Metadata?.Finalizers;
+
+        /// <summary>Gets the index of the <see cref="V1OwnerReference"/> that matches the given object, or -1 if no such
+        /// reference could be found.
+        /// </summary>
+        public static int FindOwnerReference(this IMetadata<V1ObjectMeta> obj, IKubernetesObject<V1ObjectMeta> owner) =>
+            FindOwnerReference(obj, r => r.Matches(owner));
+
+        /// <summary>Gets the index of the <see cref="V1OwnerReference"/> that matches the given predicate, or -1 if no such
+        /// reference could be found.
+        /// </summary>
+        public static int FindOwnerReference(this IMetadata<V1ObjectMeta> obj, Predicate<V1OwnerReference> predicate)
+        {
+            if (obj == null)
+            {
+                throw new ArgumentNullException(nameof(obj));
+            }
+            if (predicate == null)
+            {
+                throw new ArgumentNullException(nameof(predicate));
+            }
+
+            var ownerRefs = obj.OwnerReferences();
+            if (ownerRefs != null)
+            {
+                for (int i = 0; i < ownerRefs.Count; i++)
+                {
+                    if (predicate(ownerRefs[i])) return i;
+                }
+            }
+            return -1;
+        }
+
+        /// <summary>Gets the generation a Kubernetes object.</summary>
+        public static long? Generation(this IMetadata<V1ObjectMeta> obj) => obj.Metadata?.Generation;
+
+        /// <summary>Returns the given annotation from a Kubernetes object or null if the annotation was not found.</summary>
+        public static string GetAnnotation(this IMetadata<V1ObjectMeta> obj, string key)
+        {
+            if (obj == null)
+            {
+                throw new ArgumentNullException(nameof(obj));
+            }
+            if (key == null)
+            {
+                throw new ArgumentNullException(nameof(key));
+            }
+
+            IDictionary<string, string> annotations = obj.Annotations();
+            return annotations != null && annotations.TryGetValue(key, out string value) ? value : null;
+        }
+
+        /// <summary>Gets the <see cref="V1OwnerReference"/> for the controller of this object, or null if it couldn't be found.</summary>
+        public static V1OwnerReference GetController(this IMetadata<V1ObjectMeta> obj) =>
+            obj.OwnerReferences()?.FirstOrDefault(r => r.Controller.GetValueOrDefault());
+
+        /// <summary>Returns the given label from a Kubernetes object or null if the label was not found.</summary>
+        public static string GetLabel(this IMetadata<V1ObjectMeta> obj, string key)
+        {
+            if (obj == null)
+            {
+                throw new ArgumentNullException(nameof(obj));
+            }
+            if (key == null)
+            {
+                throw new ArgumentNullException(nameof(key));
+            }
+
+            IDictionary<string, string> labels = obj.Labels();
+            return labels != null && labels.TryGetValue(key, out string value) ? value : null;
+        }
+
+        /// <summary>Gets <see cref="V1OwnerReference"/> that matches the given object, or null if no matching reference exists.</summary>
+        public static V1OwnerReference GetOwnerReference(this IMetadata<V1ObjectMeta> obj, IKubernetesObject<V1ObjectMeta> owner) =>
+            GetOwnerReference(obj, r => r.Matches(owner));
+
+        /// <summary>Gets the <see cref="V1OwnerReference"/> that matches the given predicate, or null if no matching reference exists.</summary>
+        public static V1OwnerReference GetOwnerReference(this IMetadata<V1ObjectMeta> obj, Predicate<V1OwnerReference> predicate)
+        {
+            int index = FindOwnerReference(obj, predicate);
+            return index >= 0 ? obj.Metadata.OwnerReferences[index] : null;
+        }
+
+        /// <summary>Determines whether the Kubernetes object has the given finalizer.</summary>
+        public static bool HasFinalizer(this IMetadata<V1ObjectMeta> obj, string finalizer)
+        {
+            if (obj == null)
+            {
+                throw new ArgumentNullException(nameof(obj));
+            }
+            if (string.IsNullOrEmpty(finalizer))
+            {
+                throw new ArgumentNullException(nameof(finalizer));
+            }
+
+            return obj.Finalizers() != null && obj.Metadata.Finalizers.Contains(finalizer);
+        }
+
+        /// <summary>Determines whether one object is owned by another.</summary>
+        public static bool IsOwnedBy(this IMetadata<V1ObjectMeta> obj, IKubernetesObject<V1ObjectMeta> owner) =>
+            FindOwnerReference(obj, owner) >= 0;
+
+        /// <summary>Gets the labels of a Kubernetes object.</summary>
+        public static IDictionary<string, string> Labels(this IMetadata<V1ObjectMeta> obj) => obj.Metadata?.Labels;
+
+        /// <summary>Gets the name of a Kubernetes object.</summary>
+        public static string Name(this IMetadata<V1ObjectMeta> obj) => obj.Metadata?.Name;
+
+        /// <summary>Gets the namespace of a Kubernetes object.</summary>
+        public static string Namespace(this IMetadata<V1ObjectMeta> obj) => obj.Metadata?.NamespaceProperty;
+
+        /// <summary>Gets the owner references of a Kubernetes object.</summary>
+        public static IList<V1OwnerReference> OwnerReferences(this IMetadata<V1ObjectMeta> obj) => obj.Metadata?.OwnerReferences;
+
+        /// <summary>Removes the given finalizer from a Kubernetes object if it exists.</summary>
+        /// <returns>Returns true if the finalizer was removed and false if it didn't exist.</returns>
+        public static bool RemoveFinalizer(this IMetadata<V1ObjectMeta> obj, string finalizer)
+        {
+            if (obj == null)
+            {
+                throw new ArgumentNullException(nameof(obj));
+            }
+            if (string.IsNullOrEmpty(finalizer))
+            {
+                throw new ArgumentNullException(nameof(finalizer));
+            }
+
+            return obj.Finalizers() != null && obj.Metadata.Finalizers.Remove(finalizer);
+        }
+
+        /// <summary>Removes the first <see cref="V1OwnerReference"/> that matches the given object and returns it, or returns null if no
+        /// matching reference could be found.
+        /// </summary>
+        public static V1OwnerReference RemoveOwnerReference(this IMetadata<V1ObjectMeta> obj, IKubernetesObject<V1ObjectMeta> owner)
+        {
+            int index = FindOwnerReference(obj, owner);
+            V1OwnerReference ownerRef = index >= 0 ? obj.Metadata.OwnerReferences[index] : null;
+            if (index >= 0)
+            {
+                obj.Metadata.OwnerReferences.RemoveAt(index);
+            }
+            return ownerRef;
+        }
+
+        /// <summary>Removes all <see cref="V1OwnerReference">owner references</see> that match the given predicate, and returns true if
+        /// any were removed.
+        /// </summary>
+        public static bool RemoveOwnerReferences(this IMetadata<V1ObjectMeta> obj, Predicate<V1OwnerReference> predicate)
+        {
+            if (obj == null)
+            {
+                throw new ArgumentNullException(nameof(obj));
+            }
+            if (predicate == null)
+            {
+                throw new ArgumentNullException(nameof(predicate));
+            }
+
+            bool removed = false;
+            IList<V1OwnerReference> refs = obj.Metadata?.OwnerReferences;
+            if (refs != null)
+            {
+                for (int i = refs.Count-1; i >= 0; i--)
+                {
+                    if (predicate(refs[i]))
+                    {
+                        refs.RemoveAt(i);
+                        removed = true;
+                    }
+                }
+            }
+            return removed;
+        }
+
+        /// <summary>Removes all <see cref="V1OwnerReference">owner references</see> that match the given object, and returns true if
+        /// any were removed.
+        /// </summary>
+        public static bool RemoveOwnerReferences(this IMetadata<V1ObjectMeta> obj, IKubernetesObject<V1ObjectMeta> owner) =>
+            RemoveOwnerReferences(obj, r => r.Matches(owner));
+
+        /// <summary>Gets the resource version of a Kubernetes object.</summary>
+        public static string ResourceVersion(this IMetadata<V1ObjectMeta> obj) => obj.Metadata?.ResourceVersion;
+
+        /// <summary>Sets or removes an annotation on a Kubernetes object.</summary>
+        public static void SetAnnotation(this IMetadata<V1ObjectMeta> obj, string key, string value)
+        {
+            if (obj == null)
+            {
+                throw new ArgumentNullException(nameof(obj));
+            }
+            if (key == null)
+            {
+                throw new ArgumentNullException(nameof(key));
+            }
+
+            if (value != null)
+            {
+                obj.EnsureMetadata().EnsureAnnotations()[key] = value;
+            }
+            else
+            {
+                obj.Metadata?.Annotations?.Remove(key);
+            }
+        }
+
+        /// <summary>Sets or removes a label on a Kubernetes object.</summary>
+        public static void SetLabel(this IMetadata<V1ObjectMeta> obj, string key, string value)
+        {
+            if (obj == null)
+            {
+                throw new ArgumentNullException(nameof(obj));
+            }
+            if (key == null)
+            {
+                throw new ArgumentNullException(nameof(key));
+            }
+
+            if (value != null)
+            {
+                obj.EnsureMetadata().EnsureLabels()[key] = value;
+            }
+            else
+            {
+                obj.Metadata?.Labels?.Remove(key);
+            }
+        }
+
+        /// <summary>Gets the unique ID of a Kubernetes object.</summary>
+        public static string Uid(this IMetadata<V1ObjectMeta> obj) => obj.Metadata?.Uid;
+
+        /// <summary>Ensures that the <see cref="V1ObjectMeta.Annotations"/> field is not null, and returns it.</summary>
+        public static IDictionary<string, string> EnsureAnnotations(this V1ObjectMeta meta)
+        {
+            if (meta == null)
+            {
+                throw new ArgumentNullException(nameof(meta));
+            }
+            if (meta.Annotations == null)
+            {
+                meta.Annotations = new Dictionary<string, string>();
+            }
+            return meta.Annotations;
+        }
+
+        /// <summary>Ensures that the <see cref="V1ObjectMeta.Finalizers"/> field is not null, and returns it.</summary>
+        public static IList<string> EnsureFinalizers(this V1ObjectMeta meta)
+        {
+            if (meta == null)
+            {
+                throw new ArgumentNullException(nameof(meta));
+            }
+            if (meta.Finalizers == null)
+            {
+                meta.Finalizers = new List<string>();
+            }
+            return meta.Finalizers;
+        }
+
+        /// <summary>Ensures that the <see cref="V1ObjectMeta.Labels"/> field is not null, and returns it.</summary>
+        public static IDictionary<string, string> EnsureLabels(this V1ObjectMeta meta)
+        {
+            if (meta == null)
+            {
+                throw new ArgumentNullException(nameof(meta));
+            }
+            if (meta.Labels == null)
+            {
+                meta.Labels = new Dictionary<string, string>();
+            }
+            return meta.Labels;
+        }
+
+        /// <summary>Gets the namespace from Kubernetes metadata.</summary>
+        public static string Namespace(this V1ObjectMeta meta) => meta.NamespaceProperty;
+
+        /// <summary>Sets the namespace from Kubernetes metadata.</summary>
+        public static void SetNamespace(this V1ObjectMeta meta, string ns) => meta.NamespaceProperty = ns;
+
+        /// <summary>Determines whether an object reference references the given object.</summary>
+        public static bool Matches(this V1ObjectReference objref, IKubernetesObject<V1ObjectMeta> obj)
+        {
+            if (objref == null)
+            {
+                throw new ArgumentNullException(nameof(objref));
+            }
+            if (obj == null)
+            {
+                throw new ArgumentNullException(nameof(obj));
+            }
+            return objref.ApiVersion == obj.ApiVersion && objref.Kind == obj.Kind && objref.Name == obj.Name() && objref.Uid == obj.Uid() &&
+                   objref.NamespaceProperty == obj.Namespace();
+        }
+
+        /// <summary>Determines whether an owner reference references the given object.</summary>
+        public static bool Matches(this V1OwnerReference owner, IKubernetesObject<V1ObjectMeta> obj)
+        {
+            if (owner == null)
+            {
+                throw new ArgumentNullException(nameof(owner));
+            }
+            if (obj == null)
+            {
+                throw new ArgumentNullException(nameof(obj));
+            }
+            return owner.ApiVersion == obj.ApiVersion && owner.Kind == obj.Kind && owner.Name == obj.Name() && owner.Uid == obj.Uid();
+        }
+    }
+
     public partial class V1Status
     {
         /// <summary>Converts a <see cref="V1Status"/> object into a short description of the status.</summary>
--- tests/KubernetesClient.Tests/ModelExtensionTests.cs
+++ tests/KubernetesClient.Tests/ModelExtensionTests.cs
@@ -1,3 +1,4 @@
+using System;
 using k8s.Models;
 using Xunit;
 
@@ -5,6 +6,154 @@ namespace k8s.Tests
 {
     public class ModelExtensionTests
     {
+        [Fact]
+        public void TestMetadata()
+        {
+            // test getters on null metadata
+            var pod = new V1Pod();
+            Assert.Null(pod.Annotations());
+            Assert.Null(pod.ApiGroup());
+            var (g, v) = pod.ApiGroupAndVersion();
+            Assert.Null(g);
+            Assert.Null(v);
+            Assert.Null(pod.ApiGroupVersion());
+            Assert.Null(pod.CreationTimestamp());
+            Assert.Null(pod.DeletionTimestamp());
+            Assert.Null(pod.Finalizers());
+            Assert.Equal(-1, pod.FindOwnerReference(r => true));
+            Assert.Null(pod.Generation());
+            Assert.Null(pod.GetAnnotation("x"));
+            Assert.Null(pod.GetController());
+            Assert.Null(pod.GetLabel("x"));
+            Assert.Null(pod.GetOwnerReference(r => true));
+            Assert.False(pod.HasFinalizer("x"));
+            Assert.Null(pod.Labels());
+            Assert.Null(pod.Name());
+            Assert.Null(pod.Namespace());
+            Assert.Null(pod.OwnerReferences());
+            Assert.Null(pod.ResourceVersion());
+            Assert.Null(pod.Uid());
+            Assert.Null(pod.Metadata);
+
+            // test API version stuff
+            pod = new V1Pod() { ApiVersion = "v1" };
+            Assert.Equal("", pod.ApiGroup());
+            (g, v) = pod.ApiGroupAndVersion();
+            Assert.Equal("", g);
+            Assert.Equal("v1", v);
+            Assert.Equal("v1", pod.ApiGroupVersion());
+            pod.ApiVersion = "abc/v2";
+            Assert.Equal("abc", pod.ApiGroup());
+            (g, v) = pod.ApiGroupAndVersion();
+            Assert.Equal("abc", g);
+            Assert.Equal("v2", v);
+            Assert.Equal("v2", pod.ApiGroupVersion());
+
+            // test the Ensure*() functions
+            Assert.NotNull(pod.EnsureMetadata());
+            Assert.NotNull(pod.Metadata);
+            Assert.NotNull(pod.Metadata.EnsureAnnotations());
+            Assert.NotNull(pod.Metadata.Annotations);
+            Assert.NotNull(pod.Metadata.EnsureFinalizers());
+            Assert.NotNull(pod.Metadata.Finalizers);
+            Assert.NotNull(pod.Metadata.EnsureLabels());
+            Assert.NotNull(pod.Metadata.Labels);
+
+            // test getters with non-null values
+            DateTime ts = DateTime.UtcNow, ts2 = DateTime.Now;
+            pod.Metadata = new V1ObjectMeta()
+            {
+                CreationTimestamp = ts, DeletionTimestamp = ts2, Generation = 1, Name = "name", NamespaceProperty = "ns", ResourceVersion = "42", Uid = "id"
+            };
+            Assert.Equal(ts, pod.CreationTimestamp().Value);
+            Assert.Equal(ts2, pod.DeletionTimestamp().Value);
+            Assert.Equal(1, pod.Generation().Value);
+            Assert.Equal("name", pod.Name());
+            Assert.Equal("ns", pod.Namespace());
+            Assert.Equal("42", pod.ResourceVersion());
+            Assert.Equal("id", pod.Uid());
+
+            // test annotations and labels
+            pod.SetAnnotation("x", "y");
+            pod.SetLabel("a", "b");
+            Assert.Equal(1, pod.Annotations().Count);
+            Assert.Equal(1, pod.Labels().Count);
+            Assert.Equal("y", pod.GetAnnotation("x"));
+            Assert.Equal("y", pod.Metadata.Annotations["x"]);
+            Assert.Null(pod.GetAnnotation("a"));
+            Assert.Equal("b", pod.GetLabel("a"));
+            Assert.Equal("b", pod.Metadata.Labels["a"]);
+            Assert.Null(pod.GetLabel("x"));
+            pod.SetAnnotation("x", null);
+            Assert.Equal(0, pod.Annotations().Count);
+            pod.SetLabel("a", null);
+            Assert.Equal(0, pod.Labels().Count);
+
+            // test finalizers
+            Assert.False(pod.HasFinalizer("abc"));
+            Assert.True(pod.AddFinalizer("abc"));
+            Assert.True(pod.HasFinalizer("abc"));
+            Assert.False(pod.AddFinalizer("abc"));
+            Assert.False(pod.HasFinalizer("xyz"));
+            Assert.False(pod.RemoveFinalizer("xyz"));
+            Assert.True(pod.RemoveFinalizer("abc"));
+            Assert.False(pod.HasFinalizer("abc"));
+            Assert.False(pod.RemoveFinalizer("abc"));
+        }
+
+        [Fact]
+        public void TestReferences()
+        {
+            // test object references
+            var pod = new V1Pod() { ApiVersion = "v1", Kind = "Pod" };
+            pod.Metadata = new V1ObjectMeta() { Name = "name", NamespaceProperty = "ns", ResourceVersion = "ver", Uid = "id" };
+
+            var objr = new V1ObjectReference() { ApiVersion = pod.ApiVersion, Kind = pod.Kind, Name = pod.Name(), NamespaceProperty = pod.Namespace(), Uid = pod.Uid() };
+            Assert.True(objr.Matches(pod));
+
+            (pod.ApiVersion, pod.Kind) = (null, null);
+            Assert.False(objr.Matches(pod));
+            (pod.ApiVersion, pod.Kind) = ("v1", "Pod");
+            Assert.True(objr.Matches(pod));
+            pod.Metadata.Name = "nome";
+            Assert.False(objr.Matches(pod));
+
+            // test owner references
+            (pod.ApiVersion, pod.Kind) = ("abc/xyz", "sometimes");
+            var ownr = new V1OwnerReference() { ApiVersion = "abc/xyz", Kind = "sometimes", Name = pod.Name(), Uid = pod.Uid() };
+            Assert.True(ownr.Matches(pod));
+
+            (pod.ApiVersion, pod.Kind) = (null, null);
+            Assert.False(ownr.Matches(pod));
+            (ownr.ApiVersion, ownr.Kind) = ("v1", "Pod");
+            Assert.False(ownr.Matches(pod));
+            (pod.ApiVersion, pod.Kind) = (ownr.ApiVersion, ownr.Kind);
+            Assert.True(ownr.Matches(pod));
+            ownr.Name = "nim";
+            Assert.False(ownr.Matches(pod));
+            ownr.Name = pod.Name();
+
+            var svc = new V1Service();
+            svc.AddOwnerReference(ownr);
+            Assert.Equal(0, svc.FindOwnerReference(pod));
+            Assert.Equal(-1, svc.FindOwnerReference(svc));
+            Assert.Same(ownr, svc.GetOwnerReference(pod));
+            Assert.Null(svc.GetOwnerReference(svc));
+            Assert.Null(svc.GetController());
+            svc.OwnerReferences()[0].Controller = true;
+            Assert.Same(ownr, svc.GetController());
+            Assert.Same(ownr, svc.RemoveOwnerReference(pod));
+            Assert.Equal(0, svc.OwnerReferences().Count);
+            svc.AddOwnerReference(new V1OwnerReference() { ApiVersion = pod.ApiVersion, Kind = pod.Kind, Name = pod.Name(), Uid = pod.Uid(), Controller = true });
+            svc.AddOwnerReference(new V1OwnerReference() { ApiVersion = pod.ApiVersion, Kind = pod.Kind, Name = pod.Name(), Uid = pod.Uid(), Controller = false });
+            svc.AddOwnerReference(new V1OwnerReference() { ApiVersion = pod.ApiVersion, Kind = pod.Kind, Name = pod.Name(), Uid = pod.Uid() });
+            Assert.Equal(3, svc.OwnerReferences().Count);
+            Assert.NotNull(svc.RemoveOwnerReference(pod));
+            Assert.Equal(2, svc.OwnerReferences().Count);
+            Assert.True(svc.RemoveOwnerReferences(pod));
+            Assert.Equal(0, svc.OwnerReferences().Count);
+        }
+
         [Fact]
         public void TestV1Status()
         {
