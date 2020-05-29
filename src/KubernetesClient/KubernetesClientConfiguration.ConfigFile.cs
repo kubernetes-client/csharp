187ec8fb18d61feb50a8d54b90bb218a09e49500
Kyle J. Fischer <kylefischer1995@gmail.com>
2020-05-29 14:32:34 -0400

    Removed NETSTANDARD2_0 IFDefs and added Brackets (#451)
    * Removed NETSTANDARD2_0 IFDefs and Fixed some brackets it wouldn't let me build if I didn't fix them
    
    * Removed bracket changes
    
    * Fixed Formatting hopefully
    
    * Updated StartInfo.EnvironmentVariables from StartInfo.Environment so it is compatiable with 4.5.2
    
    * Forgot I had changed this and didn't check git diff till now. My Bad
    
    * Missed a spot where it was referencing Envrionment and not EnvironmentVariables

--- src/KubernetesClient/KubernetesClientConfiguration.ConfigFile.cs
+++ src/KubernetesClient/KubernetesClientConfiguration.ConfigFile.cs
@@ -1,9 +1,7 @@
 using System;
 using System.Collections.Generic;
-#if NETSTANDARD2_0
 using Newtonsoft.Json;
 using System.Diagnostics;
-#endif
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices;
@@ -378,7 +376,6 @@ namespace k8s
                 }
             }
 
-#if NETSTANDARD2_0
             if (userDetails.UserCredentials.ExternalExecution != null)
             {
                 if (string.IsNullOrWhiteSpace(userDetails.UserCredentials.ExternalExecution.Command))
@@ -397,7 +394,6 @@ namespace k8s
 
                 userCredentialsFound = true;
             }
-#endif
 
             if (!userCredentialsFound)
             {
@@ -411,7 +407,6 @@ namespace k8s
             throw new KubeConfigException("Refresh not supported.");
         }
 
-#if NETSTANDARD2_0
         /// <summary>
         /// Implementation of the proposal for out-of-tree client
         /// authentication providers as described here --
@@ -432,14 +427,12 @@ namespace k8s
 
             var process = new Process();
 
-            process.StartInfo.Environment.Add("KUBERNETES_EXEC_INFO",
-                JsonConvert.SerializeObject(execInfo));
-
+            process.StartInfo.EnvironmentVariables.Add("KUBERNETES_EXEC_INFO", JsonConvert.SerializeObject(execInfo));
             if (config.EnvironmentVariables != null)
             {
                 foreach (var configEnvironmentVariableKey in config.EnvironmentVariables.Keys)
                 {
-                    process.StartInfo.Environment.Add(key: configEnvironmentVariableKey,
+                    process.StartInfo.EnvironmentVariables.Add(key: configEnvironmentVariableKey,
                         value: config.EnvironmentVariables[configEnvironmentVariableKey]);
                 }
             }
@@ -493,7 +486,6 @@ namespace k8s
                 throw new KubeConfigException($"external exec failed due to uncaught exception: {ex}");
             }
         }
-#endif
 
         /// <summary>
         ///     Loads entire Kube Config from default or explicit file path
