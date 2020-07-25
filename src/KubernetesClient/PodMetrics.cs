6c7aba97b4fb38e883b69fce9d8af0354aaa475d
alarco-l <contact@ludovic-alarcon.com>
2020-07-25 16:30:11 +0200

    Add models for pod metrics

---
+++ src/KubernetesClient/PodMetrics.cs
@@ -0,0 +1,36 @@
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+
+namespace k8s.Models
+{
+    /// <summary>
+    /// Describes the resource usage metrics of a pod pull from metrics server API.
+    /// </summary>
+    public class PodMetrics
+    {
+        /// <summary>
+        /// The kubernetes standard object's metadata.
+        /// </summary>
+        [JsonProperty(PropertyName = "metadata")]
+        public V1ObjectMeta Metadata { get; set; }
+
+        /// <summary>
+        /// The timestamp when metrics were collected.
+        /// </summary>
+        [JsonProperty(PropertyName = "timestamp")]
+        public DateTime Timestamp { get; set; }
+
+        /// <summary>
+        /// The interval from which metrics were collected.
+        /// </summary>
+        [JsonProperty(PropertyName = "window")]
+        public string Window { get; set; }
+
+        /// <summary>
+        /// The list of containers metrics.
+        /// </summary>
+        [JsonProperty(PropertyName = "containers")]
+        public List<ContainerMetrics> Containers { get; set; }
+    }
+}
