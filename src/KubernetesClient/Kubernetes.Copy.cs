5e88e0209ba197b03452e97458692653e6a5bbd5
alarco-l <contact@ludovic-alarcon.com>
2020-08-13 23:30:37 +0200

    refactor using tar like kubectl cp

--- src/KubernetesClient/Kubernetes.Copy.cs
+++ src/KubernetesClient/Kubernetes.Copy.cs
@@ -1,62 +1,82 @@
-using k8s.Models;
 using System;
 using System.IO;
+using System.Security.Cryptography;
 using System.Threading;
 using System.Threading.Tasks;
+using ICSharpCode.SharpZipLib.GZip;
+using ICSharpCode.SharpZipLib.Tar;
+using k8s.Models;
 
 namespace k8s
 {
     public partial class Kubernetes
     {
-        public async Task CopyFileFromPod(V1Pod pod, string container, string sourcePath, string destinationPath, CancellationToken cancellationToken = default(CancellationToken))
+        /*******************************************************************
+        ** /!\ Requires that the 'tar' binary is present in your container
+        ** image. If 'tar' is not present, the copy will fail. /!\
+        *******************************************************************/
+        public async Task<int> CopyFileFromPodAsync(V1Pod pod, string container, string sourcePath, string destinationPath, CancellationToken cancellationToken = default(CancellationToken))
         {
-            await CopyFileFromPod(pod.Metadata.Name, pod.Metadata.NamespaceProperty, container, sourcePath, destinationPath, cancellationToken).ConfigureAwait(false);
+            if (pod == null)
+            {
+                throw new ArgumentNullException(nameof(pod));
+            }
+
+            return await CopyFileFromPodAsync(pod.Metadata.Name, pod.Metadata.NamespaceProperty, container, sourcePath, destinationPath, cancellationToken).ConfigureAwait(false);
         }
 
-        public async Task CopyFileFromPod(string name, string @namespace, string container, string sourceFilePath, string destinationFilePath, CancellationToken cancellationToken = default(CancellationToken))
+        public async Task<int> CopyFileFromPodAsync(string name, string @namespace, string container, string sourceFilePath, string destinationFilePath, CancellationToken cancellationToken = default(CancellationToken))
         {
-            var handler = GetCopyFileFromPodHandler(destinationFilePath);
-
             // All other parameters are being validated by MuxedStreamNamespacedPodExecAsync called by NamespacedPodExecAsync
             if (string.IsNullOrWhiteSpace(sourceFilePath))
             {
                 throw new ArgumentException($"{nameof(sourceFilePath)} cannot be null or whitespace");
             }
+
             if (string.IsNullOrWhiteSpace(destinationFilePath))
             {
                 throw new ArgumentException($"{nameof(destinationFilePath)} cannot be null or whitespace");
             }
 
-            await NamespacedPodExecAsync(name, @namespace, container, new string[] { "sh", "-c", $"cat {sourceFilePath} | base64" }, false, handler, cancellationToken).ConfigureAwait(false);
-        }
-
-        private ExecAsyncCallback GetCopyFileFromPodHandler(string destinationFilePath)
-        {
-            return new ExecAsyncCallback((stdIn, stdOut, stdError) =>
+            // The callback which processes the standard input, standard output and standard error of exec method
+            var handler = new ExecAsyncCallback(async (stdIn, stdOut, stdError) =>
             {
-                StreamReader errorReader = new StreamReader(stdError);
-
-                if (errorReader.Peek() != -1)
+                using (var errorReader = new StreamReader(stdError))
                 {
-                    var error = errorReader.ReadToEnd();
-                    throw new IOException($"Copy command failed: {error}");
+                    if (errorReader.Peek() != -1)
+                    {
+                        var error = await errorReader.ReadToEndAsync().ConfigureAwait(false);
+                        throw new IOException($"Copy command failed: {error}");
+                    }
                 }
 
-                StreamReader outReader = new StreamReader(stdOut);
-                var encodedData = outReader.ReadToEnd();
-
-                using (FileStream output = new FileStream(destinationFilePath, FileMode.Create))
+                try
                 {
-                    var data = Convert.FromBase64String(encodedData);
-                    output.Write(data, 0, data.Length);
-                }
+                    using (var stream = new CryptoStream(stdOut, new FromBase64Transform(), CryptoStreamMode.Read))
+                    using (var gzipStream = new GZipInputStream(stream))
+                    using (var tarInputStream = new TarInputStream(gzipStream))
+                    {
+                        var tarEntry = tarInputStream.GetNextEntry();
+                        var directoryName = Path.GetDirectoryName(destinationFilePath);
 
-#if NET452
-                return Task.FromResult(0);
-#else
-                return Task.CompletedTask;
-#endif
+                        if (!string.IsNullOrEmpty(directoryName))
+                        {
+                            Directory.CreateDirectory(directoryName);
+                        }
+
+                        using (var outputFile = new FileStream(destinationFilePath, FileMode.Create))
+                        {
+                            tarInputStream.CopyEntryContents(outputFile);
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    throw new IOException($"Copy command failed: {ex.Message}");
+                }
             });
+
+            return await NamespacedPodExecAsync(name, @namespace, container, new string[] { "sh", "-c", $"tar cz {sourceFilePath} | base64" }, false, handler, cancellationToken).ConfigureAwait(false);
         }
     }
 }
