475f75e0655b988c8c1421717bb408b68f16521e
alarco-l <contact@ludovic-alarcon.com>
2020-08-14 16:25:50 +0200

    Add copy file to pod

--- src/KubernetesClient/IKubernetes.Copy.cs
+++ src/KubernetesClient/IKubernetes.Copy.cs
@@ -54,5 +54,54 @@ namespace k8s
         /// A <see cref="Task"/> which represents the asynchronous operation.
         /// </returns>
         Task<int> CopyFileFromPodAsync(string name, string @namespace, string container, string sourceFilePath, string destinationFilePath, CancellationToken cancellationToken = default(CancellationToken));
+
+        /// <summary>
+        /// Copy a file from a node into the local machine.
+        /// </summary>
+        /// <param name="pod">
+        /// The pod object which contains the container in which the source file exist.
+        /// </param>
+        /// <param name="container">
+        /// The container in which the source file exist.
+        /// </param>
+        /// <param name="sourceFilePath">
+        /// The source file path.
+        /// </param>
+        /// <param name="destinationFilePath">
+        /// The destination file path.
+        /// </param>
+        /// <param name="cancellationToken">
+        /// A <see cref="CancellationToken"/> which can be used to cancel the asynchronous operation.
+        /// </param>
+        /// <returns>
+        /// A <see cref="Task"/> which represents the asynchronous operation.
+        /// </returns>
+        Task<int> CopyFileToPodAsync(V1Pod pod, string container, string sourceFilePath, string destinationFilePath, CancellationToken cancellationToken = default(CancellationToken));
+
+        /// <summary>
+        /// Copy a file from a node into the local machine.
+        /// </summary>
+        /// <param name="name">
+        /// The name of the pod which contains the container in which the source file exist.
+        /// </param>
+        /// <param name="namespace">
+        /// The namespace of the container.
+        /// </param>
+        /// <param name="container">
+        /// The container in which the source file exist.
+        /// </param>
+        /// <param name="sourceFilePath">
+        /// The source file path.
+        /// </param>
+        /// <param name="destinationFilePath">
+        /// The destination file path.
+        /// </param>
+        /// <param name="cancellationToken">
+        /// A <see cref="CancellationToken"/> which can be used to cancel the asynchronous operation.
+        /// </param>
+        /// <returns>
+        /// A <see cref="Task"/> which represents the asynchronous operation.
+        /// </returns>
+        Task<int> CopyFileToPodAsync(string name, string @namespace, string container, string sourceFilePath, string destinationFilePath, CancellationToken cancellationToken = default(CancellationToken));
     }
 }
--- src/KubernetesClient/Kubernetes.Copy.cs
+++ src/KubernetesClient/Kubernetes.Copy.cs
@@ -1,8 +1,9 @@
 using System;
 using System.IO;
 using System.Security.Cryptography;
-using System.Threading;
+using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
 using ICSharpCode.SharpZipLib.GZip;
 using ICSharpCode.SharpZipLib.Tar;
 using k8s.Models;
@@ -77,6 +78,107 @@ namespace k8s
             });
 
             return await NamespacedPodExecAsync(name, @namespace, container, new string[] { "sh", "-c", $"tar cz {sourceFilePath} | base64" }, false, handler, cancellationToken).ConfigureAwait(false);
+        }
+
+        public async Task<int> CopyFileToPodAsync(V1Pod pod, string container, string sourcePath, string destinationPath, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            if (pod == null)
+            {
+                throw new ArgumentNullException(nameof(pod));
+            }
+
+            return await CopyFileToPodAsync(pod.Metadata.Name, pod.Metadata.NamespaceProperty, container, sourcePath, destinationPath, cancellationToken).ConfigureAwait(false);
+        }
+
+        public async Task<int> CopyFileToPodAsync(string name, string @namespace, string container, string sourceFilePath, string destinationFilePath, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            // All other parameters are being validated by MuxedStreamNamespacedPodExecAsync called by NamespacedPodExecAsync
+            if (string.IsNullOrWhiteSpace(sourceFilePath))
+            {
+                throw new ArgumentException($"{nameof(sourceFilePath)} cannot be null or whitespace");
+            }
+
+            if (string.IsNullOrWhiteSpace(destinationFilePath))
+            {
+                throw new ArgumentException($"{nameof(destinationFilePath)} cannot be null or whitespace");
+            }
+
+            // The callback which processes the standard input, standard output and standard error of exec method
+            var handler = new ExecAsyncCallback(async (stdIn, stdOut, stdError) =>
+            {
+                var fileInfo = new FileInfo(destinationFilePath);
+
+                try
+                {
+                    using (var outputStream = new MemoryStream())
+                    {
+                        using (var inputFileStream = File.OpenRead(sourceFilePath))
+                        using (var gZipOutputStream = new GZipOutputStream(outputStream))
+                        using (var tarOutputStream = new TarOutputStream(gZipOutputStream))
+                        {
+                            // To avoid gZipOutputStream to close the memoryStream
+                            gZipOutputStream.IsStreamOwner = false;
+
+                            var fileSize = inputFileStream.Length;
+                            var entry = TarEntry.CreateTarEntry(fileInfo.Name);
+                            entry.Size = fileSize;
+
+                            tarOutputStream.PutNextEntry(entry);
+
+                            // this is copied from TarArchive.WriteEntryCore
+                            byte[] localBuffer = new byte[32 * 1024];
+                            while (true)
+                            {
+                                int numRead = inputFileStream.Read(localBuffer, 0, localBuffer.Length);
+                                if (numRead <= 0)
+                                {
+                                    break;
+                                }
+
+                                tarOutputStream.Write(localBuffer, 0, numRead);
+                            }
+
+                            tarOutputStream.CloseEntry();
+                        }
+
+                        outputStream.Position = 0;
+                        using (var cryptoStream = new CryptoStream(stdIn, new ToBase64Transform(), CryptoStreamMode.Write))
+                        {
+                            outputStream.CopyTo(cryptoStream);
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    throw new IOException($"Copy command failed: {ex.Message}");
+                }
+
+                using (var errorReader = new StreamReader(stdError))
+                {
+                    if (errorReader.Peek() != -1)
+                    {
+                        var error = await errorReader.ReadToEndAsync().ConfigureAwait(false);
+                        throw new IOException($"Copy command failed: {error}");
+                    }
+                }
+            });
+
+            var destinationFileInfo = new FileInfo(destinationFilePath);
+            var destinationFolder = Path.GetDirectoryName(destinationFilePath);
+
+            if (string.IsNullOrEmpty(destinationFolder))
+            {
+                destinationFolder = ".";
+            }
+
+            return await NamespacedPodExecAsync(
+                name,
+                @namespace,
+                container,
+                new string[] { "sh", "-c", $"base64 -d | tar xzmf - -C {destinationFolder}" },
+                false,
+                handler,
+                cancellationToken).ConfigureAwait(false);
         }
     }
 }
