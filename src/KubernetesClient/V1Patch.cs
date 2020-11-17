e18ec35250972359e17ce896dd1f522f113eec54
Brendan Burns <bburns@microsoft.com>
2020-11-17 05:40:04 -0800

    fix patch example. (#517)

--- src/KubernetesClient/V1Patch.cs
+++ src/KubernetesClient/V1Patch.cs
@@ -7,25 +7,36 @@ namespace k8s.Models
     [JsonConverter(typeof(V1PathJsonConverter))]
     public partial class V1Patch
     {
-        public enum PathType
+        public enum PatchType
         {
             JsonPatch,
             MergePatch,
             StrategicMergePatch,
         }
 
-        public PathType Type { get; private set; }
+        public PatchType Type { get; private set; }
 
         public V1Patch(IJsonPatchDocument jsonPatch)
             : this((object)jsonPatch)
         {
         }
 
+        public V1Patch(String body, PatchType type)
+            : this(body)
+        {
+            this.Type = type;
+        }
+
         partial void CustomInit()
         {
             if (Content is IJsonPatchDocument)
             {
-                Type = PathType.JsonPatch;
+                Type = PatchType.JsonPatch;
+                return;
+            }
+
+            if (Content is String)
+            {
                 return;
             }
 
