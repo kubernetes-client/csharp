77796dc47f2823db600ea6567781952a118c96b8
David Dieruf <david@dierufventures.com>
2021-09-14 20:32:56 -0400

    Remove unneeded json property

--- src/KubernetesClient/Util/Common/Generic/Options/ListOptions.cs
+++ src/KubernetesClient/Util/Common/Generic/Options/ListOptions.cs
@@ -4,22 +4,16 @@ namespace k8s.Util.Common.Generic.Options
 {
     public class ListOptions
     {
-        [JsonProperty("timeoutSeconds")]
         public int? TimeoutSeconds { get; private set; }
 
-        [JsonProperty("limit")]
         public int Limit { get; private set; }
 
-        [JsonProperty("fieldSelector")]
         public string FieldSelector { get; private set; }
 
-        [JsonProperty("labelSelector")]
         public string LabelSelector { get; private set; }
 
-        [JsonProperty("resourceVersion")]
         public string ResourceVersion { get; private set; }
 
-        [JsonProperty("continue")]
         public string Continue { get; private set; }
 
         public ListOptions(int? timeoutSeconds = default, int limit = default, string fieldSelector = default, string labelSelector = default, string resourceVersion = default,
