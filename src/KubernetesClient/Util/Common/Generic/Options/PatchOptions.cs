5be9295ecf42ff5fad72b109da55ec6cc9cd16aa
David Dieruf <david@dierufventures.com>
2021-09-14 21:38:28 -0400

    Remove unneeded json properties

--- src/KubernetesClient/Util/Common/Generic/Options/PatchOptions.cs
+++ src/KubernetesClient/Util/Common/Generic/Options/PatchOptions.cs
@@ -4,13 +4,10 @@ namespace k8s.Util.Common.Generic.Options
 {
     public class PatchOptions
     {
-        [JsonProperty("dryRun")]
         public string DryRun { get; private set; }
 
-        [JsonProperty("force")]
         public bool Force { get; private set; }
 
-        [JsonProperty("fieldManager")]
         public string FieldManager { get; private set; }
 
         public PatchOptions(string dryRun = default, bool force = false, string fieldManager = default)
--- src/KubernetesClient/Util/Common/Generic/Options/UpdateOptions.cs
+++ src/KubernetesClient/Util/Common/Generic/Options/UpdateOptions.cs
@@ -4,13 +4,10 @@ namespace k8s.Util.Common.Generic.Options
 {
     public class UpdateOptions
     {
-        [JsonProperty("dryRun")]
         public string DryRun { get; private set; }
 
-        [JsonProperty("force")]
         public bool Force { get; private set; }
 
-        [JsonProperty("fieldManager")]
         public string FieldManager { get; private set; }
 
         public UpdateOptions(string dryRun = default, bool force = false, string fieldManager = default)
