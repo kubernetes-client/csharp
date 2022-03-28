dba7b9718e7c9f5a6ac7da87b602f2784ed2af7c
Boshi Lian <farmer1992@gmail.com>
2022-03-28 08:45:23 -0700

    move getheader to abstract (#807)

--- src/KubernetesClient.Basic/AbstractKubernetes.cs
+++ src/KubernetesClient.Basic/AbstractKubernetes.cs
@@ -2,6 +2,7 @@ using System.Net.Http;
 using System.Threading;
 using System.Threading.Tasks;
 using k8s.Autorest;
+using k8s.Models;
 using System.Net.Http.Headers;
 
 
@@ -61,11 +62,46 @@ namespace k8s
 
         public virtual TimeSpan HttpClientTimeout { get; set; } = TimeSpan.FromSeconds(100);
 
+        protected virtual MediaTypeHeaderValue GetHeader(object body)
+        {
+            if (body == null)
+            {
+                throw new ArgumentNullException(nameof(body));
+            }
+
+            if (body is V1Patch patch)
+            {
+                return GetHeader(patch);
+            }
+
+            return MediaTypeHeaderValue.Parse("application/json; charset=utf-8");
+        }
+
+        private MediaTypeHeaderValue GetHeader(V1Patch body)
+        {
+            if (body == null)
+            {
+                throw new ArgumentNullException(nameof(body));
+            }
+
+            switch (body.Type)
+            {
+                case V1Patch.PatchType.JsonPatch:
+                    return MediaTypeHeaderValue.Parse("application/json-patch+json; charset=utf-8");
+                case V1Patch.PatchType.MergePatch:
+                    return MediaTypeHeaderValue.Parse("application/merge-patch+json; charset=utf-8");
+                case V1Patch.PatchType.StrategicMergePatch:
+                    return MediaTypeHeaderValue.Parse("application/strategic-merge-patch+json; charset=utf-8");
+                case V1Patch.PatchType.ApplyPatch:
+                    return MediaTypeHeaderValue.Parse("application/apply-patch+yaml; charset=utf-8");
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(body.Type), "");
+            }
+        }
+
         protected abstract Task<HttpOperationResponse<T>> CreateResultAsync<T>(HttpRequestMessage httpRequest, HttpResponseMessage httpResponse, bool? watch, CancellationToken cancellationToken);
 
         protected abstract HttpRequestMessage CreateRequest(string relativeUri, string method, IDictionary<string, IList<string>> customHeaders);
-
-        protected abstract MediaTypeHeaderValue GetHeader(object body);
 
         protected abstract Task<HttpResponseMessage> SendRequestRaw(string requestContent, HttpRequestMessage httpRequest, CancellationToken cancellationToken);
     }
