e3ffd6e607ee517859f2d1a011c284e94c917307
Boshi Lian <farmer1992@gmail.com>
2022-05-07 08:50:56 -0700

    fix vis to

--- src/KubernetesClient.Basic/AssemblyInfo.cs
+++ src/KubernetesClient.Basic/AssemblyInfo.cs
@@ -2,5 +2,4 @@ using System.Runtime.CompilerServices;
 
 [assembly: InternalsVisibleTo("KubernetesClient")]
 [assembly: InternalsVisibleTo("KubernetesClient.Classic")]
-[assembly: InternalsVisibleTo("KubernetesClient.VanillaRest")]
 [assembly: InternalsVisibleTo("KubernetesClient.Tests")]
--- src/KubernetesClient.Models/AssemblyInfo.cs
+++ src/KubernetesClient.Models/AssemblyInfo.cs
@@ -1,5 +1,6 @@
 using System.Runtime.CompilerServices;
 
 [assembly: InternalsVisibleTo("KubernetesClient")]
+[assembly: InternalsVisibleTo("KubernetesClient.Classic")]
 [assembly: InternalsVisibleTo("KubernetesClient.Basic")]
 [assembly: InternalsVisibleTo("KubernetesClient.Tests")]
