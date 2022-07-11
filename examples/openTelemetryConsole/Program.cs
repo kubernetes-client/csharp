1ea0fe16e89aa6bbba99d1141232d273cc74eb82
Manuel Menegazzo <mmenegazzo@battistellacompany.it>
2022-07-11 14:24:24 +0200

    Improved minimal example of OpenTelemetry console

--- examples/openTelemetryConsole/Program.cs
+++ examples/openTelemetryConsole/Program.cs
@@ -1,46 +1,21 @@
 using k8s;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using OpenTelemetry.Exporter;
+using OpenTelemetry;
 using OpenTelemetry.Resources;
 using OpenTelemetry.Trace;
-using System.Reflection;
 
-var builder = Host.CreateDefaultBuilder(args);
+var serviceName = "MyCompany.MyProduct.MyService";
+var serviceVersion = "1.0.0";
 
-builder.ConfigureServices((builder, services) =>
-{
-    // Register OpenTelemetry TraceProvider
-    services.AddOpenTelemetryTracing(tpb =>
-    {
-        var programName = Assembly.GetExecutingAssembly().GetName().Name!;
-        var programVersion = Assembly.GetExecutingAssembly().GetName().Version?.ToString() ?? "unknown";
-
-        tpb.AddSource(programName);
-        tpb.SetResourceBuilder(ResourceBuilder.CreateDefault().AddService(serviceName: programName, serviceVersion: programVersion));
-        tpb.AddHttpClientInstrumentation();
-
-        tpb.AddJaegerExporter(options =>
-        {
-            options.AgentHost = "http://localhost";
-            options.AgentPort = 6831;
-            options.Protocol = JaegerExportProtocol.UdpCompactThrift;
-        });
-    });
-
-    // Register IHttpClientFactory
-    services.AddHttpClient();
-});
-
-// Build the DI Container
-var app = builder.Build();
-
-
+// Create the traceProvide with HttpClient instrumentation enabled
+using var tracerProvider = Sdk.CreateTracerProviderBuilder()
+    .AddSource(serviceName)
+    .SetResourceBuilder(ResourceBuilder.CreateDefault().AddService(serviceName: serviceName, serviceVersion: serviceVersion))
+    .AddHttpClientInstrumentation()
+    .AddConsoleExporter()
+    .Build();
 
 // Load kubernetes configuration
 var config = KubernetesClientConfiguration.BuildDefaultConfig();
-// Create the httpClient
-var httpClient = app.Services.GetService<IHttpClientFactory>().CreateClient();
 
 // Create an istance of Kubernetes client
 IKubernetes client = new Kubernetes(config);
@@ -48,12 +23,12 @@ IKubernetes client = new Kubernetes(config);
 // Read the list of pods contained in default namespace
 var list = client.CoreV1.ListNamespacedPod("default");
 
-// Print the name of pods
+// Print the name of pods 
 foreach (var item in list.Items)
 {
     Console.WriteLine(item.Metadata.Name);
 }
-
+// Or empty if there are no pods
 if (list.Items.Count == 0)
 {
     Console.WriteLine("Empty!");
