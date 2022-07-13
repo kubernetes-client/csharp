39e7d98c36f4ba62e0dd32b500bc551ca80015bf
Manuel Menegazzo <65919883+m3nax@users.noreply.github.com>
2022-07-14 01:36:56 +0200

    ASP.NET core web api DI example (#950)
    * Added example source code
    
    * Removed unnecessary nuget package

---
+++ examples/webApiDependencyInjection/Program.cs
@@ -0,0 +1,34 @@
+using k8s;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Load kubernetes configuration
+var kubernetesClientConfig = KubernetesClientConfiguration.BuildDefaultConfig();
+
+// Register Kubernetes client interface as sigleton
+builder.Services.AddSingleton<IKubernetes>(new Kubernetes(kubernetesClientConfig));
+
+// Add services to the container.
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddControllers();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+// Start the service
+app.Run();
+
+
+// Swagger ui can be accesse at: http://localhost:<yourBindingPort>/swagger
