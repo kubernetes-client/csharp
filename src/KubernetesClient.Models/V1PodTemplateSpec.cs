138cb8627f4077adcf4ce13d784942138f2d1908
stan-sz <37585349+stan-sz@users.noreply.github.com>
2022-09-07 11:39:09 +0000

    Typo

--- src/KubernetesClient.Models/V1PodTemplateSpec.cs
+++ src/KubernetesClient.Models/V1PodTemplateSpec.cs
@@ -4,7 +4,7 @@ namespace k8s.Models
     /// Partial implementation of the IMetadata interface
     /// to open this class up to ModelExtensions methods
     /// </summary>
-    public partial class V1PodTemplateSpec : 997<V1ObjectMeta>
+    public partial class V1PodTemplateSpec : IMetadata<V1ObjectMeta>
     {
     }
 }
