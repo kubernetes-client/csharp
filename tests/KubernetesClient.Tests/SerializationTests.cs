a7824f7593f5b98454f153cf1389d5afe1fc8c9b
Luis Cantero <lcantero@microsoft.com>
2023-04-02 18:40:22 +0200

    Add test

---
+++ tests/KubernetesClient.Tests/SerializationTests.cs
@@ -0,0 +1,51 @@
+using k8s.Tests.Mock;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace k8s.Tests
+{
+    public class SerializationTests
+    {
+        private readonly ITestOutputHelper testOutput;
+
+        private enum Animals
+        {
+            Dog,
+            Cat,
+            Mouse,
+        }
+
+        public SerializationTests(ITestOutputHelper testOutput)
+        {
+            this.testOutput = testOutput;
+        }
+
+        [Fact]
+        public async Task SerializeEnumUsingCamelCase()
+        {
+            using (var server = new MockKubeApiServer(testOutput))
+            {
+                var client = new Kubernetes(
+                    new KubernetesClientConfiguration { Host = server.Uri.ToString() },
+                    options =>
+                    {
+                        // Insert the converter at the front of the list so it overrides
+                        // the default JsonStringEnumConverter without namingPolicy.
+                        options.Converters.Insert(index: 0, new JsonStringEnumConverter(JsonNamingPolicy.CamelCase));
+                    });
+
+                var customObject = Animals.Dog;
+
+                var result = await client.CustomObjects.CreateNamespacedCustomObjectWithHttpMessagesAsync(customObject, "TestGroup", "TestVersion", "TestNamespace", "TestPlural").ConfigureAwait(false);
+                var content = await result.Request.Content.ReadAsStringAsync();
+                Assert.Equal(@"""dog""", content);
+
+                string animal = KubernetesJson.Serialize(Animals.Cat);
+                Assert.Equal(@"""cat""", animal);
+            }
+        }
+    }
+}
