fb6e396cad062ca2eb7f4b6e6888beb8f6f0e150
Boshi LIAN <bolian@microsoft.com>
2024-01-14 04:18:39 -0800

    add e2e test for aot

--- src/KubernetesClient.Aot/SourceGenerationContext.cs
+++ src/KubernetesClient.Aot/SourceGenerationContext.cs
@@ -5,7 +5,7 @@ namespace k8s;
 [JsonSourceGenerationOptions(
     DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull,
     PropertyNamingPolicy = JsonKnownNamingPolicy.CamelCase,
-    Converters = new[] { typeof(Iso8601TimeSpanConverter), typeof(KubernetesDateTimeConverter), typeof(KubernetesDateTimeOffsetConverter), typeof(JsonStringEnumConverter<WatchEventType>) })
+    Converters = new[] { typeof(Iso8601TimeSpanConverter), typeof(KubernetesDateTimeConverter), typeof(KubernetesDateTimeOffsetConverter) })
     ]
 internal partial class SourceGenerationContext : JsonSerializerContext
 {
