faebb4748699aba34c7b8a100a821642043fe1c9
Boshi LIAN <bolian@microsoft.com>
2025-09-09 16:22:50 -0700

    refactor: reorganize Kubernetes client models into separate files

---
+++ src/KubernetesClient/Models/IItems.cs
@@ -0,0 +1,27 @@
+namespace k8s.Models;
+
+/// <summary>
+/// Kubernetes object that exposes list of objects
+/// </summary>
+/// <typeparam name="T">type of the objects</typeparam>
+public interface IItems<T>
+{
+    /// <summary>
+    /// Gets or sets list of objects. More info:
+    /// https://git.k8s.io/community/contributors/devel/sig-architecture/api-conventions.md
+    /// </summary>
+    IList<T> Items { get; set; }
+}
+
+public static class ItemsExt
+{
+    public static IEnumerator<T> GetEnumerator<T>(this IItems<T> items)
+    {
+        if (items is null)
+        {
+            throw new ArgumentNullException(nameof(items));
+        }
+
+        return items.Items.GetEnumerator();
+    }
+}
