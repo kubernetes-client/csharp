66d51e33a9147f180669473ce9c9b36eb67fcf52
Ivan Josipovic <9521987+IvanJosipovic@users.noreply.github.com>
2025-10-21 21:50:56 -0700

    enable UseStringEnumConverter

--- src/KubernetesClient/SourceGenerationContext.cs
+++ src/KubernetesClient/SourceGenerationContext.cs
@@ -5,6 +5,7 @@ namespace k8s;
 [JsonSourceGenerationOptions(
     DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull,
     PropertyNamingPolicy = JsonKnownNamingPolicy.CamelCase,
+    UseStringEnumConverter = true,
     Converters = new[] { typeof(Iso8601TimeSpanConverter), typeof(KubernetesDateTimeConverter), typeof(KubernetesDateTimeOffsetConverter), typeof(V1Status.V1StatusObjectViewConverter) })
     ]
 public partial class SourceGenerationContext : JsonSerializerContext
