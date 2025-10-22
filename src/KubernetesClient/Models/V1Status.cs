b526a1ee01266b9086408fb23738fea75ae62064
Ivan Josipovic <9521987+IvanJosipovic@users.noreply.github.com>
2025-10-22 14:18:59 -0700

    wip

--- src/KubernetesClient/Models/V1Status.cs
+++ src/KubernetesClient/Models/V1Status.cs
@@ -2,7 +2,6 @@ using System.Net;
 
 namespace k8s.Models
 {
-    [JsonConverter(typeof(V1StatusObjectViewConverter))]
     public partial record V1Status
     {
         /// <summary>Converts a <see cref="V1Status"/> object into a short description of the status.</summary>
