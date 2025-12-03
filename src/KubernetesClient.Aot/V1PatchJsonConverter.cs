4659415e766e0bb3936b9af0ad9c8f1611453710
Ivan Josipovic <9521987+IvanJosipovic@users.noreply.github.com>
2025-12-02 18:10:03 -0800

    chore: remove uneeded pragma warning

--- src/KubernetesClient.Aot/V1PatchJsonConverter.cs
+++ src/KubernetesClient.Aot/V1PatchJsonConverter.cs
@@ -1,8 +1,6 @@
 namespace k8s.Models
 {
-#pragma warning disable CA1812 // Avoid uninstantiated internal classes
     public sealed class V1PatchJsonConverter : JsonConverter<V1Patch>
-#pragma warning restore CA1812 // Avoid uninstantiated internal classes
     {
         public override V1Patch Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
         {
